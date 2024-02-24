d7efa07fba3496e947f1f31fb163b7596e7677f4
Vadim Lemmer <lemmeronne@gmail.com>
2024-02-24 13:03:45 +0600

    feat: добавлена оброботка строки

---
+++ unreal_practice_1_14.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string line;
+
+    std::cout << "Enter string: ";
+    std::getline(std::cin, line);
+
+    std::cout << "String length: " << line.size() << "\n";
+    std::cout << "First character: " << line.front() << "\n";
+    std::cout << "Last character: " << line.back() << "\n";
+}
